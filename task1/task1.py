7fb82af83b66681432c0089c81ec6cc70f420610
gau <gau@1jz.ru>
2024-08-28 23:55:00 +0300

    refactoring task1 and task2

--- task1/task1.py
+++ task1/task1.py
@@ -2,20 +2,16 @@ def task1(n: int, m: int) -> str:
     n_i = 0
     m_i = 0
     track = '1'
-    # log = ''
     while True:
         n_i += 1
         m_i += 1
-        # log += str(n_i)
         if n_i == 1 and m_i == m:
             break
         elif m_i == m and n_i != 1:
-            # log += f',{n_i}'
             m_i = 1
             track += str(n_i)
         if n_i == n:
             n_i = 0
-    # print(log)
     return track
 
 
